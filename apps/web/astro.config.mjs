194d500f90ec1c02cf135579306ff97c45f3e009
mrnosilrub <eburlison@me.com>
2025-08-14 20:34:05 -0500

    Add changes

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -13,6 +13,10 @@ export default {
           target: 'http://localhost:8000',
           changeOrigin: true,
         },
+        '/v1': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+        },
       },
     },
   },
