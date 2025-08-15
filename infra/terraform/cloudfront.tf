71ce491ee73523bd6f336321e1a068a75ae502ff
mrnosilrub <eburlison@me.com>
2025-08-15 16:44:45 -0500

    devops(sprint10): add CDN plan + CloudFront terraform stub; prep for env contexts

---
+++ infra/terraform/cloudfront.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket backing the CDN"
+  type        = string
+}
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "txwl-oac"
+  description                       = "Origin access for reports/tiles"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = "${var.bucket_name}.s3.amazonaws.com"
+    origin_id                = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+    min_ttl     = 0
+    default_ttl = 86400
+    max_ttl     = 31536000
+  }
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cdn_domain_name" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+
